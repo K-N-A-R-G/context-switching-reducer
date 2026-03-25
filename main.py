bb475d818901a873aaf966b8ea7e302522d2adff
Knarg <knarg0000@gmail.com>
2026-03-25 22:32:38 +0200

    Update: synchronization with core module

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+# --- SYNC DATA BLOCK: ASYNCIO ---
+__all__ = (base_events.__all__ +
+           coroutines.__all__ +
+           events.__all__ +
+           exceptions.__all__ +
+           futures.__all__ +
+           locks.__all__ +
+
+# --- END OF NODE UPDATE ---
